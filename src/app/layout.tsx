ba072a1c76e22eb6791d37e157ff1c58e98cf945
Jiraded <jay@jenosize.com>
2024-07-25 10:18:27 +0700

    change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={font.className} suppressHydrationWarning={true}>
         <InitialLanguage />
         <Intitalize />
-        <div className="relative flex min-h-[calc(100vh)] w-full justify-center overflow-auto bg-[#f2f1e9]">
+        <div className="relative flex min-h-[calc(100vh)] w-full justify-center overflow-auto bg-[#39465a]">
           <div className="fixed z-[3] h-[68px] w-full bg-[#2f3745] outline outline-4 outline-[#585e68]" />
           <div className="relative w-full max-w-maxw bg-[#39465a] py-4">
             <Menu />
